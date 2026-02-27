6f0d81ddfea67058427c74b1c35a02b872e767e0
Rishav Bharadwaj <your_github_email@gmail.com>
2026-02-27 13:31:43 +0530

    Fixed database creation for Render

--- app.py
+++ app.py
@@ -207,7 +207,13 @@ def rishav():
 # ================= RUN LOCAL SERVER =================
 
 if __name__ == "__main__":
+   db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
     app.run(
+
         host="0.0.0.0",
         port=5000,
         debug=False,
